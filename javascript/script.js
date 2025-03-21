d46a643bcb8e98e6a41248ae00c95842cd9031ec
Ahmad Farisi <rachmat.nur@gmail.com>
2025-03-21 17:26:03 +0700

    fetch API

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,22 @@ let nilai = [
     {nama: "Ernest", nilai: "A"},
     {nama: "Michael", nilai: "A-"}
 ];
+nilai.forEach( (data) => {
+    document.getElementById('nilai').innerHTML += `<tr>
+        <td> ${data.nama} </td>
+        <td> ${data.nilai} </td>
+    </tr>`;
+})
+
+// Public API data gempa BMKG
+fetch('https://data.bmkg.go.id/DataMKG/TEWS/gempaterkini.json')
+.then( response => response.json()) // ubah jadi format JSON
+.then( data => {
+    // console.log(data);
+    console.log(data.Infogempa.gempa);
+    data.Infogempa.gempa.forEach( (data) => {
+        document.getElementById('gempa').innerHTML += `<li>
+            ${data.Wilayah} ${data.Tanggal} ${data.Jam} ${data.Potensi}      
+        </li>`
+    } )
+})
