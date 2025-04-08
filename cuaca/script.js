502f7c05859a4b74ea0c02e7254464c92048b175
Nur Rachmat <rachmat.nur@gmail.com>
2025-04-08 18:09:23 +0700

    api cuaca

---
+++ cuaca/script.js
@@ -0,0 +1,21 @@
+// akses API desa kelurahan
+fetch("https://24252-if21-pw1-omega.vercel.app/javascript/wilayah.json")
+.then( response => response.json() )
+.then( data => {
+    console.log(data);
+    data.forEach( (item) => {
+        document.getElementById("list-desa").innerHTML += `<li onclick='detail("${item.kode}")'> ${item.kode} ${item.nama } </li>`
+    })
+} )
+
+function detail(kode) {
+    console.log(kode);
+    // akses API cuaca BMKG berdasarkan kode wilayah desa/kelurahan
+    fetch(`https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4=${kode}`)
+    .then( response => response.json() )
+    .then( data => {
+        console.log(data);
+        document.getElementById('desa').innerHTML = data.lokasi.desa;
+        document.getElementById('kecamatan').innerHTML = data.lokasi.kecamatan;
+    } )
+}
